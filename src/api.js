52b6d876d203e0e67db1bd30a137aaf4f724ea1f
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-11-20 12:41:22 +0100

    imported nprogress

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import mockData from "./mock-data";
+import { NProgress } from "nprogress";
 
 // * This function takes an events array, then uses map to create a new array with only locations.
 // * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
